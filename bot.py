f69a69b2ebdd530502d71cc2902a672f40513a65
meprabhas02 <meprabhas02@gmail.com>
2026-03-06 21:07:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import requests
 import feedparser
 import schedule
 import time
+import os
+from flask import Flask
+import threading
 
-TOKEN = "YOUR_BOT_TOKEN"
-CHANNEL_ID = "-100XXXXXXXX"
+TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 RSS_URL = "https://www.filmibeat.com/rss/filmibeat-movies-fb.xml"
 
@@ -15,6 +18,7 @@ def check_news():
 
     for entry in feed.entries[:5]:
         if entry.link not in posted:
+
             message = f"🎬 Movie Update\n\n{entry.title}\n\nRead more:\n{entry.link}"
 
             url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -28,6 +32,21 @@ def check_news():
 
 schedule.every(10).minutes.do(check_news)
 
-while True:
-    schedule.run_pending()
-    time.sleep(5)
+def run_bot():
+    while True:
+        schedule.run_pending()
+        time.sleep(5)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Telegram Movie Bot Running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_bot).start()
+
+run_web()
